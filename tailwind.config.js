7f03391c3af4a022ec19019ebfe96257ef06126d
subhradwi01 <subhradwi01@gmail.com>
2023-04-14 20:17:56 +0530

    theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{html,js,jsx}'
   ],
   theme: {
     extend: {},
+    colors:{
+      bgPrimary:"",
+      bgSecondary:"",
+      primary:"",
+      secondary:"#ebecfd",
+      textPrimary:"#232438",
+      textSecondary:"#a9a9b8",
+      danger:"#ff0000"
+
+    }
   },
   plugins: [],
 }
-
