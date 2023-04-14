15b95c31eb5fad54c90c70372d161b19111106f3
subhradwi01 <subhradwi01@gmail.com>
2023-04-14 23:39:08 +0530

    Color added more

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,21 @@ module.exports = {
   theme: {
     extend: {},
     colors:{
-      bgPrimary:"",
-      bgSecondary:"",
-      primary:"",
+      bgPrimary:"#ffffff",
+      bgSecondary:"#f9f9f9",
+      primary:"#6166f1",
       secondary:"#ebecfd",
       textPrimary:"#232438",
       textSecondary:"#a9a9b8",
-      danger:"#ff0000"
+      danger:"#ff0000",
+      gradientEnd:"#9092FF",
+      gradinetInitial:"rgba(144, 146, 255, 0)"
 
     }
   },
   plugins: [],
 }
+
+/* *{
+  background: linear-gradient(270deg, #9092FF 1.26%, rgba(144, 146, 255, 0) 100%);
+} */
