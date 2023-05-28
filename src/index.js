9ec637943cf09ea3d43febe729ec1d63855d55de
Subhradwip Kulavi <88609894+subhradwip01@users.noreply.github.com>
2023-05-28 23:27:23 +0530

    Analytics fixed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
       <App />
-      {/* <Analytics /> */}
+       <Analytics />
   </React.StrictMode>
 );
 
