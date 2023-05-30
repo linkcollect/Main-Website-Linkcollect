7bc53f964cc871d4fe8daadf946e54ea731f3530
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-05-31 01:50:11 +0530

    made the new login/signup page accordingly,

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,7 +14,16 @@ const Login = ({ handleSetUser, windowWidth }) => {
   const location = useLocation();
   const navigate = useNavigate();
   const [isLogging, setIsLogging] = useState(false);
+  const [data, setData] = useState({
+    email: '',
+    password: ''
+  })
 
+  // Login details
+  const onInput = (e) => {
+    e.preventDefault();
+    setData(state => ({ ...state, [e.target.name]: e.target.value }));
+  };
   // For google auth redirect and email verification redirect from server with token in query
   useEffect(() => {
 
@@ -47,78 +56,94 @@ const Login = ({ handleSetUser, windowWidth }) => {
 
   return (
     <>
-      <div className="bg-gradient-to-r from-gradinetInitial to-gradientEnd from-50% ">
-        <div className="flex flex-col items-center justify-evenly min-h-screen gap-0 pb-12 lg:gap-0 sm:flex-wrap lg:flex-nowrap sm:flex-row md:justify-center md:gap-12 lg:justify-evenly max-w-[2000px] mx-auto">
-          {windowWidth > 600 && <Banner />}
-          {windowWidth < 600 &&
-            <Link to={'/'}>
-              <div className="flex items-center justify-center w-full">
-                <img src={mainLogo} alt="" className="h-16 w-36" />
-              </div>
-            </Link>
-          }
-          <div className="flex items-center justify-center w-[90%] sm:w-2/3 md:w-1/2 max-w-[420px] sm:max-w-[600px] md:max-w-[420px] ">
-            <div className="rounded-2xl bg-bgPrimary shadow-2xl px-10 pt-[40px] pb-[60px] w-full md:w-[410px]">
-              <div>
-                <img
-                  className="h-16 mx-auto w-36"
-                  src={mainLogo}
-                  alt="LinkCollect"
-                />
-                <h2 className="mt-2 mb-3 text-xl font-bold tracking-tight text-center sm:text-3xl">
-                  Welcome
-                </h2>
-                <p
-                  className="-mt-2 text-sm text-center sm:text-lg para"
-                  style={{ color: "#747474" }}
-                >
-                  Log in to LinkCollect
-                </p>
-              </div>
-              <form onSubmit={handleLogin}>
-                <div className="mt-8 mb-3">
-                  <div className="mb-3">
-                    <Input
-                      id="email"
-                      name="emial"
-                      type="email"
-                      placeholder="homelander@gmail.com"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <Input
-                      id="password"
-                      name="password"
-                      type="password"
-                      placeholder="password"
-                    />
-                  </div>
+      <div style={{ background: `linear-gradient(0deg, #9092FF -10.03%, rgba(144, 146, 255, 0) 98.39%), #FFFFFF` }}>
+        <div className="flex flex-col items-center justify-evenly min-h-screen gap-0 sm:flex-wrap lg:flex-nowrap sm:flex-row md:justify-center md:gap-12 lg:justify-evenly max-w-[3000px] mx-auto">
 
-                  {/* Need to add link after adding the api for forget pass */}
-                  {/* <p className="mb-4 font-light text-left text-textSecondary">
-                  Forget Your Password?
-                </p> */}
-                  <button className="flex justify-center w-full py-3 font-bold rounded-lg bg-primary text-bgPrimary">
-                    {!isLogging ? "Login" : <Loader />}
-                  </button>
-                  <p className="mt-1 font-light text-left whitespace-break-spaces text-textSecondary">
-                    Don't have an account?{" "}
-                    <Link to="/signup" className="font-bold text-primary">
-                      Sign Up
-                    </Link>
+
+          {windowWidth > 600 &&
+            <div className="flex items-center justify-center w-full h-[80vh] lg:min-h-screen lg:w-1/2 "
+            >
+              <Banner />
+            </div>
+          }
+
+          {/* {windowWidth < 600 &&
+            <div className="flex items-center justify-center h-16 w-36">
+              <img src={mainLogo} alt="" className="h-16 w-36" />
+            </div>
+          } */}
+          <div className="flex items-center justify-center w-full h-screen sm:py-10 lg:py-0 sm:h-auto lg:h-screen sm:items-start lg:w-1/2 lg:items-center bg-bgPrimary">
+            <div className=" flex items-center justify-center w-[90%] sm:w-2/3  lg:mt-0 md:w-3/4 lg:w-2/3 max-w-[420px] sm:max-w-[600px] md:max-w-[420px] ">
+              <div className="rounded-2xl bg-bgPrimary  px-10 pt-[40p]  pb-[60px] w-full md:w-[410px]">
+                <div>
+                  <Link to={'/'}>
+                    <img
+                      className="h-16 mx-auto w-36"
+                      src={mainLogo}
+                      alt="LinkCollect"
+                    />
+                  </Link>
+
+                  <h2 className="mt-2 mb-3 text-xl font-bold tracking-tight text-center sm:text-3xl">
+                    Welcome
+                  </h2>
+                  <p
+                    className="-mt-2 text-sm text-center sm:text-lg para"
+                    style={{ color: "#747474" }}
+                  >
+                    Log in to LinkCollect
                   </p>
                 </div>
-              </form>
-              <hr className="mt-4 hr-text" data-content="OR" />
-              {windowWidth < 600 ?
-                <div className="w-full">
+                <form onSubmit={handleLogin}>
+                  <div className="mt-8 mb-3">
+                    <div className="mb-3">
+                      <Input
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="homelander@gmail.com"
+                        value={data.email}
+                        onInput={onInput}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <Input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="password"
+                        value={data.password}
+                        onInput={onInput}
+                      />
+                    </div>
+
+                    {/* Need to add link after adding the api for forget pass */}
+                    {/* <p className="mb-4 font-light text-left text-textSecondary">
+                  Forget Your Password?
+                </p> */}
+                    <button className={`flex justify-center w-full py-3 font-bold rounded-lg ${data.email.length > 1 && data.password.length > 0 ? 'bg-primary' : 'bg-textSecondary'} text-bgPrimary`}>
+                      {!isLogging ? "Login" : <Loader />}
+                    </button>
+                    <p className="mt-1 font-light text-left whitespace-break-spaces text-textSecondary">
+                      Don't have an account?{" "}
+                      <Link to="/signup" className="font-bold text-primary">
+                        Sign Up
+                      </Link>
+                    </p>
+                  </div>
+                </form>
+                <hr className="mt-4 hr-text" data-content="OR" />
+                {windowWidth < 600 ?
+                  <div className="w-full">
+                    <GoogleAuthBtn />
+                  </div>
+                  :
                   <GoogleAuthBtn />
-                </div>  
-                :
-                <GoogleAuthBtn />
-            }
+                }
+              </div>
             </div>
           </div>
+
         </div>
       </div>
     </>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -8,18 +8,31 @@ import { register } from "../api-services/authService";
 import Emailsent from "../components/EmialVerified/Emailsent";
 import Loader from "../components/Loader/Loader";
 
-const Signup = ({windowWidth}) => {
+const Signup = ({ windowWidth }) => {
   const [verifying, setVerifying] = useState(false)
-  const [isSiging,setIsSigning] = useState(false)
+  const [isSiging, setIsSigning] = useState(false)
 
-  const handleRegister = async(e)=>{
+  // signup details handling
+  const [data, setData] = useState({
+    name: '',
+    password: '',
+    email: ''
+  })
+
+  // Login details
+  const onInput = (e) => {
+    e.preventDefault();
+    setData(state => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  const handleRegister = async (e) => {
     e.preventDefault();
     setIsSigning(true);
     try {
-      
-      const {name, email, password} = e.target;
-  
-      const {data} = await register(name.value, email.value, password.value.trim());
+
+      const { name, email, password } = e.target;
+
+      const { data } = await register(name.value, email.value, password.value.trim());
       setIsSigning(false);
       setVerifying(true);
     } catch (error) {
@@ -29,85 +42,103 @@ const Signup = ({windowWidth}) => {
 
   return (
     <>
-      <div className="bg-gradient-to-r from-gradinetInitial to-gradientEnd from-50%">
-      <div className="flex flex-col items-center justify-evenly min-h-screen gap-2 pb-12 lg:gap-0 sm:flex-wrap lg:flex-nowrap sm:flex-row md:justify-center md:gap-12 lg:justify-evenly max-w-[2000px] mx-auto">
-        {windowWidth > 600 && <Banner />}
-          {windowWidth < 600 &&
-            <Link to={'/'}>
-              <div className="flex items-center justify-center w-full">
-                <img src={mainLogo} alt="" className="h-16 w-36" />
-              </div>
-            </Link>
-          }          {!verifying?
-            <div className="flex items-center justify-center w-[90%] sm:w-2/3 md:w-1/2 max-w-[420px] sm:max-w-[600px] md:max-w-[420px] ">
-            <div className="rounded-2xl bg-bgPrimary shadow-2xl px-10 pt-[40px] pb-[60px] w-full md:w-[410px]">
-              <div>
-                <img
-                  className="h-16 mx-auto w-36"
-                  src={mainLogo}
-                  alt="LinkCollect"
-                />
-                <h2 className="mt-2 mb-3 text-xl font-bold tracking-tight text-center sm:text-3xl">
-                  Welcome
-                </h2>
-                <p
-                  className="-mt-2 text-sm text-center sm:text-lg para"
-                  style={{ color: "#747474" }}
-                >
-                  Sign Up to LinkCollect
-                </p>
-              </div>
-              <div className="mt-8 mb-3">
-                <form onSubmit={handleRegister}>
-                <div className="mb-3">
-                  <Input
-                    id="name"
-                    name="name"
-                    type="text"
-                    placeholder="Name (Water White)"
-                  />
-                </div>
-                <div className="mb-3">
-                  <Input
-                    id="email"
-                    name="emial"
-                    type="email"
-                    placeholder="homelander@gmail.com"
-                  />
-                </div>
-                <div className="mb-4">
-                  <Input
-                    id="password"
-                    name="password"
-                    type="password"
-                    placeholder="password"
-                  />
-                </div>
+      <div style={{ background: `linear-gradient(0deg, #9092FF -10.03%, rgba(144, 146, 255, 0) 98.39%), #FFFFFF` }}>
+        <div className="flex flex-col items-center justify-evenly min-h-screen gap-0 sm:flex-wrap lg:flex-nowrap sm:flex-row md:justify-center md:gap-12 lg:justify-evenly max-w-[3000px] mx-auto">
 
-                {/* Need to add link after adding the api for forget pass */}
-                {/* <p className="mb-4 font-light text-left text-textSecondary">
+          {windowWidth > 600 &&
+            <div className="flex items-center justify-center w-full h-[80vh] lg:min-h-screen lg:w-1/2 "
+            >
+              <Banner />
+            </div>
+          }
+
+          {/* {windowWidth < 600 &&
+            <div className="flex items-center justify-center w-full">
+              <img src={mainLogo} alt="" className="h-16 w-36" />
+            </div>
+          } */}
+
+
+          {!verifying ?
+            <div className="flex items-center justify-center w-full h-screen sm:py-10 lg:py-0 sm:h-auto lg:h-screen sm:items-start lg:w-1/2 lg:items-center bg-bgPrimary">
+            <div className=" flex items-center justify-center w-[90%] sm:w-2/3  lg:mt-0 md:w-3/4 lg:w-2/3 max-w-[420px] sm:max-w-[600px] md:max-w-[420px] ">
+                <div className="rounded-2xl bg-bgPrimary  px-10 pb-[60px] w-full md:w-[410px]">
+                  <div>
+                    <Link to={'/'}>
+                      <img
+                        className="h-16 mx-auto w-36"
+                        src={mainLogo}
+                        alt="LinkCollect"
+                      />
+                    </Link>
+                    <h2 className="mt-2 mb-3 text-xl font-bold tracking-tight text-center sm:text-3xl">
+                      Welcome
+                    </h2>
+                    <p
+                      className="-mt-2 text-sm text-center sm:text-lg para"
+                      style={{ color: "#747474" }}
+                    >
+                      Sign Up to LinkCollect
+                    </p>
+                  </div>
+                  <div className="mt-8 mb-3">
+                    <form onSubmit={handleRegister}>
+                      <div className="mb-3">
+                        <Input
+                          id="name"
+                          name="name"
+                          type="text"
+                          placeholder="Name (Water White)"
+                          onInput={onInput}
+                          value={data.name}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <Input
+                          id="email"
+                          name="email"
+                          type="email"
+                          placeholder="homelander@gmail.com"
+                          onInput={onInput}
+                          value={data.email}
+                        />
+                      </div>
+                      <div className="mb-4">
+                        <Input
+                          id="password"
+                          name="password"
+                          type="password"
+                          placeholder="password"
+                          onInput={onInput}
+                          value={data.password}
+                        />
+                      </div>
+
+                      {/* Need to add link after adding the api for forget pass */}
+                      {/* <p className="mb-4 font-light text-left text-textSecondary">
                   Forget Your Password?
                 </p> */}
 
-                <button className="flex items-center justify-center w-full py-3 font-bold rounded-lg bg-primary text-bgPrimary">
-                  {!isSiging ? "Sign Up" : <Loader/>}
-                </button>
-                <p className="mt-1 font-light text-left text-textSecondary">
-                  Already have an account?{" "}
-                  <Link to="/login" className="font-bold text-primary">
-                    Log in
-                  </Link>
-                </p>
-                </form>
+                      <button className={`flex items-center justify-center w-full py-3 font-bold rounded-lg ${data.name.length >1 && data.email.length > 1 && data.password.length > 1 ? 'bg-primary' : 'bg-textSecondary'} text-bgPrimary`}>
+                        {!isSiging ? "Sign Up" : <Loader />}
+                      </button>
+                      <p className="mt-1 font-light text-left text-textSecondary">
+                        Already have an account?{" "}
+                        <Link to="/login" className="font-bold text-primary">
+                          Log in
+                        </Link>
+                      </p>
+                    </form>
+                  </div>
+                  <hr className="mt-4 hr-text" data-content="OR" />
+                  <GoogleAuthBtn />
+                </div>
               </div>
-              <hr className="mt-4 hr-text" data-content="OR" />
-              <GoogleAuthBtn />
             </div>
-          </div>
-          :
-        <div>
-          <Emailsent/>
-        </div>}
+            :
+            <div>
+              <Emailsent />
+            </div>}
         </div>
       </div>
     </>
