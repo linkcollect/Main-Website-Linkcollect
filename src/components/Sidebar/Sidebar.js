c68dd51f3abf622f92fe52676bc9ba39613a4d08
sahil-develops <zambanisahil99@gmail.com>
2023-07-06 14:18:31 +0530

    Sidebar Done

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,20 +1,27 @@
 import React, { useRef, useState } from "react";
 import mainlogo from "../../assets/mainLogo.svg";
-import ProfileImage from "../../assets/profile.jpeg";
+// import ProfileImage from "../../assets/profile.jpeg";
 import StackIcon from "../../assets/stack-simple.svg";
 import ArrowIcon from "../../assets/back-arrow.svg";
 import AddIcon from "../../assets/add-tab.svg";
 import LinkIcon from "../../assets/link.svg";
 import Logout from "../../assets/logout.svg";
-import approve from "../../assets/approve.svg"
+import approve from "../../assets/approve.svg";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import LinkCopied from '../../assets/LinkCopied.svg'
+import LinkCopied from "../../assets/LinkCopied.svg";
+import defaultImage from "../../assets/defaultImage.svg";
+import home from "../../assets/homeSidebar.svg";
+import bookmark from "../../assets/bmSidebar.svg";
+import settings from "../../assets/settingsSidebar.svg";
+import navigation from "../../assets/navSidebar.svg";
+import energy from "../../assets/energy.svg";
+
 const Sidebar = ({ user, handleSetUser, windowWidth }) => {
   const copyRef = useRef();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [showCollections, setShowCollections] = useState(false);
-  const [isCopied, setIsCopied] = useState(false)
+  const [isCopied, setIsCopied] = useState(false);
   const handleShowCollection = () => {
     setShowCollections(!showCollections);
   };
@@ -23,50 +30,87 @@ const Sidebar = ({ user, handleSetUser, windowWidth }) => {
       copyRef.current.src = approve;
     }
     navigator.clipboard.writeText("https://linkcollect.io/" + user.username);
-    setIsCopied(true)
+    setIsCopied(true);
     setTimeout(() => {
-      copyRef.current.src = LinkIcon
-      setIsCopied(false)
+      copyRef.current.src = LinkIcon;
+      setIsCopied(false);
     }, 1500);
   };
 
   const logoutHandler = () => {
     localStorage.removeItem("token");
-    handleSetUser(null, null, false)
-    return navigate("/")
-  }
+    handleSetUser(null, null, false);
+    return navigate("/");
+  };
   return (
-    <aside className={` bg-bgPrimary ${windowWidth < 700 ? "h-[350px] w-full " : 'w-[305px]'} `}>
-      <div className={`flex flex-col top-0 items-center justify-between h-[100vh] w-full px-2 ${user ? '' : 'pb-12'} `}>
+    <aside
+      className={` bg-[#F3F3F6] border-r-[1px] border-[#D1D1DB] ${
+        windowWidth < 700 ? "h-[350px] w-full " : "w-[270px]"
+      } `}
+    >
+      <div
+        className={`flex flex-col top-0 items-center justify-between h-[100vh] w-full  ${
+          user ? "" : "pb-2"
+        } `}
+      >
         {/* Profile Info */}
         <div className="flex flex-col gap-8">
-          <Link to='/'>
+          <Link to="/">
             <img src={mainlogo} alt="" className="w-40 mx-auto" />
           </Link>
-          <div className="w-full">
-            <div className="w-[90px] h-[73px] rounded-2xl mx-auto mb-2 overflow-hidden">
+          <div className="w-full border-2  border-[#D1D1DB] rounded-lg py-3 px-3 ">
+            <div className=" h-[100px] w-[100px] mx-auto mb-2 overflow-hidden">
               <img
-                src={ProfileImage}
-                className="w-[90px] h-[73px] object-cover"
-
+                src={defaultImage}
+                className="h-full w-full rounded-full object-cover"
               />
             </div>
-            <p className="font-bold text-[16px]">{user.isLoggedIn && user.isOwner ? user.username : user.name}</p>
-            {user.email && <p className="font-light text-[16px] pt-1">{user.email}</p>}
+            <p className="font-bold text-[16px]">
+              {user.isLoggedIn && user.isOwner ? user.username : user.name}
+            </p>
+            {/* {user.email && <p className="font-light text-[16px] pt-1">{user.email}</p>} */}
+
+            <div className="flex flex-col justify-evenly items-start text-base font-normal text-[#4B4C63] gap-2 pt-8">
+              <div className="flex flex-row justify-between items-center gap-[118px] text-xs">
+                <p>Link Saved</p>
+                <p className="ml-1">
+                  {user.link?.privateCollection + user.link?.publicCollection ||
+                    0}
+                </p>
+              </div>
+              <div className="flex flex-row justify-between items-center gap-24 text-xs whitespace-nowrap">
+                <p>Total Collection</p>
+                <p>50</p>
+              </div>
+            </div>
+          </div>
+
+          {/* Tabs */}
+          <div className="flex justify-start flex-col items-start gap-4">
+            <div className="flex flex-row justify-start gap-3 items-center w-full bg-[#DADBFF] cursor-pointer border-1 rounded-md py-3 px-2 ">
+              <img src={home} /> <span className="text-base text-[#6166F1] ">Home </span> 
+            </div>
+            <div className="flex flex-row justify-start gap-3 items-center w-full hover:bg-[#DADBFF] cursor-pointer border-1 rounded-md py-3 px-2 ">
+              <img src={navigation} /> Explore{" "}
+            </div>
+            <div className="flex flex-row justify-start gap-3 items-center w-full hover:bg-[#DADBFF] cursor-pointer border-1 rounded-md py-3 px-2 ">
+              <img src={bookmark} /> Saved Collection
+            </div>
+            <div className="flex flex-row justify-start gap-3 items-center w-full hover:bg-[#DADBFF] cursor-pointer border-1 rounded-md py-3 px-2 ">
+              <img src={settings} /> Settings
+            </div>
           </div>
 
           {/* Collection priavaci info */}
-          <div className={`${windowWidth < 700 ? " flex items-center justify-center gap-2 w-full" : ""}`}>
+          {/* <div className={`${windowWidth < 700 ? " flex items-center justify-center gap-2 w-full" : ""}`}>
             <div className={`${windowWidth < 700 ? "w-[163px]" : "w-[281px]"} h-[41px]  rounded-lg font-bold text-textPrimary border-[1px] ${user ? 'border-borderPrimary' : 'border-primary'}  py-1 flex items-center justify-between px-2`}>
 
               <div className="flex items-center gap-2 ">
                 <img src={StackIcon} alt="" className="w-5 h-5" />
                 <span className={`text-textPrimary text-[13px] sm:text-[16px] w-[60px] h-[23px] sm:h-[25px]`}>Collections</span>
-              </div>
+              </div> */}
 
-
-
-              <div className="flex items-center justify-center gap-2">
+          {/* <div className="flex items-center justify-center gap-2">
 
                 <span
                   className={`font-normal w-5 h-5 leading-5 text-[14px] sm:text-[16px] text-primary `}
@@ -85,11 +129,10 @@ const Sidebar = ({ user, handleSetUser, windowWidth }) => {
                     onClick={handleShowCollection}
                   />
                 }
-              </div>
-
-            </div>
-            {/* for responsive button */}
-            <button onClick={onCopy} className={` w-[160px]  border-borderPrimary border-[1px] font-bold text-textPrimary   rounded-lg py-2 flex justify-center items-center gap-[8px] sm:gap-1 mt-1 mb-1 ${windowWidth < 700 ? "block" : "hidden"} relative`}>
+              </div> */}
+        </div>
+        {/* for responsive button */}
+        {/* <button onClick={onCopy} className={` w-[160px]  border-borderPrimary border-[1px] font-bold text-textPrimary   rounded-lg py-2 flex justify-center items-center gap-[8px] sm:gap-1 mt-1 mb-1 ${windowWidth < 700 ? "block" : "hidden"} relative`}>
               <span className="text-[14px]">Share Profile</span>
               <img ref={copyRef} src={LinkIcon} alt="" className="w-4" />
               {isCopied &&
@@ -97,9 +140,9 @@ const Sidebar = ({ user, handleSetUser, windowWidth }) => {
                 <img alt='LinkCopied' src={LinkCopied} />
               </div>
             }
-            </button>
-            {/* problem aarha ha tha when adjusting width of collections in mobile devicess so added the logic of windowWidth */}
-            <div className={`h-[65px] w-[281px] ${windowWidth < 700 ? "hidden" : 'block'}`}>
+            </button> */}
+        {/* problem aarha ha tha when adjusting width of collections in mobile devicess so added the logic of windowWidth */}
+        {/* <div className={`h-[65px] w-[281px] ${windowWidth < 700 ? "hidden" : 'block'}`}>
               {showCollections && (
                 <div className="flex flex-col py-3 mx-auto cursor-pointer bg-bgSecondary">
                   <div className="flex items-center justify-between px-2 pb-3">
@@ -112,19 +155,18 @@ const Sidebar = ({ user, handleSetUser, windowWidth }) => {
                   </div>
                 </div>
               )}
-            </div>
-          </div>
-        </div>
+            </div> */}
+        {/* </div>
+        </div> */}
         {/* Action buttons */}
-        <div className="w-full">
-          {/* {user?.isOwner &&
+        {/* {user?.isOwner &&
             <button className="flex items-center justify-center w-full gap-2 py-2 font-bold rounded-lg bg-primary text-bgPrimary">
               <img src={AddIcon} alt="" className="w-4" />
               <span className="text-[14px]">Create Collection</span>
             </button>
           } */}
-          {/* Share Profile */}
-          <button onClick={onCopy} className={`w-full font-bold text-textPrimary border-[1px] ${'border-primary'} rounded-lg py-2 flex justify-center items-center gap-1 mt-1 mb-1 ${windowWidth < 700 ? "hidden" : ""} relative`}>
+        {/* Share Profile */}
+        {/* <button onClick={onCopy} className={`w-full font-bold text-textPrimary border-[1px] ${'border-primary'} rounded-lg py-2 flex justify-center items-center gap-1 mt-1 mb-1 ${windowWidth < 700 ? "hidden" : ""} relative`}>
             <span className="text-[14px]">Share Profile</span>
             <img ref={copyRef} src={LinkIcon} alt="" className="w-4" />
             {isCopied &&
@@ -132,23 +174,18 @@ const Sidebar = ({ user, handleSetUser, windowWidth }) => {
                 <img alt='LinkCopied' src={LinkCopied} />
               </div>
             }
-          </button>
-          {user?.isOwner &&
-            <>
-              {/* <div className="w-full font-bold text-textPrimary border-2 border-[#ededed] rounded-lg py-2 flex justify-center items-center gap-1 mt-1">
-                <span className="text-[14px]">Public</span>
-                <img src={Link} alt="" className="w-4" />
-              </div> */}
-              <button className="flex items-center justify-center w-full gap-1 py-4 bg-white cursor-pointer " onClick={logoutHandler}>
-                <img src={Logout} alt="" className="w-5 h-5" />
-                <span
-                  className="text-danger para  text-[12px] leading-[12px]"                >
-                  Logout
-                </span>
-              </button>
-            </>
-          }
+          </button> */}
 
+        {/* Upgrade button */}
+
+        <div className="flex justify-start items-center my-5 mx-5">
+          <button
+            className={` font-normal  text-[#fff] outline-none px-20 py-3   bg-primary  rounded-lg  flex justify-start items-center gap-4  ${
+              windowWidth < 700 ? "hidden" : ""
+            } relative`}
+          >
+            <img src={energy} width={16} /> Upgrade
+          </button>
         </div>
       </div>
     </aside>
@@ -156,35 +193,3 @@ const Sidebar = ({ user, handleSetUser, windowWidth }) => {
 };
 
 export default Sidebar;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
