fd248a5328ea91997c573de16956c55faa849c4c
subhradwi01 <subhradwi01@gmail.com>
2023-07-12 21:54:40 +0530

    remobved unnecessary functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,9 +12,6 @@ export const getOrigin = (weblink) =>{
     }
 }
 
-export const getAddedTime = (time)=>{
-
-}
 
 export const dataSortByType = (data,sortingType)=>{
     
@@ -28,32 +25,7 @@ export const dataSortByType = (data,sortingType)=>{
     }
 
 }
-
-
-const DURATION_IN_SECONDS = {
-    epochs: ['year', 'month', 'day', 'hour', 'minute'],
-    year: 31536000,
-    month: 2592000,
-    day: 86400,
-    hour: 3600,
-    minute: 60
-  };
   
-const getDuration = (seconds) => {
-    var epoch, interval;
-  
-    for (var i = 0; i < DURATION_IN_SECONDS.epochs.length; i++) {
-      epoch = DURATION_IN_SECONDS.epochs[i];
-      interval = Math.floor(seconds / DURATION_IN_SECONDS[epoch]);
-      if (interval >= 1) {
-        return {
-          interval: interval,
-          epoch: epoch
-        };
-      }
-    }
-  
-  };
   
 export const fromNow = (date)=>{
    let seconds = Math.floor((Date.now() - new Date(date)) / 1000);
