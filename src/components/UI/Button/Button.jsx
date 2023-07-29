ada893e7ab395fa612c7b88593365c23e9a4f0eb
subhradwi01 <subhradwi01@gmail.com>
2023-07-29 17:59:39 +0530

    Reuasble button added

---
+++ src/components/UI/Button/Button.jsx
@@ -0,0 +1,56 @@
+import React, { forwardRef } from "react";
+import { cva } from "class-variance-authority";
+import { classMerge } from "../../../utils/utils";
+
+const ButtonVariants = cva(
+  " flex w-full items-center justify-center cursor-pointer ",
+  {
+    variants: {
+      variant: {
+        primary: " bg-primary-500 text-white ",
+        primaryOutlined:
+          " bg-neutral-100 border-2 border-primary-500 text-primary-500 ",
+        secondaryOutline: "bg-neutral-200 border border-neutral-300 text-neutral-700",
+        error: " bg-error-500 text-white ",
+        errorOutlined:
+          " bg-neutral-200 border-2 border-error-500 text-error-500 ",
+        disabled: " bg-neutral-400 text-white cursor-not-allowed ",
+        loading: " cursor-not-allowed ",
+      },
+      size: {
+        default:
+          " h-12 rounded-xl px-3 py-6 px-3 py-6 font-medium text-[16px] ",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "default",
+    },
+  }
+);
+
+const Button = forwardRef(
+  ({
+    className,
+    size,
+    variant: buttonVariant,
+    disabled,
+    isLoading,
+    ...props
+  }) => {
+    const variant =
+      !disabled && !isLoading
+        ? buttonVariant
+        : disabled
+        ? "disabled"
+        : "loading";
+    return (
+      <button
+        className={classMerge(ButtonVariants({ variant, size, className }))}
+        {...props}
+      />
+    );
+  }
+);
+
+export default Button;
