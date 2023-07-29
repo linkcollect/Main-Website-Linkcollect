ebf8ed02570bdfa845abf47f26b986c53904abd5
subhradwi01 <subhradwi01@gmail.com>
2023-07-29 17:59:57 +0530

    Reusable chip added

---
+++ src/components/UI/Chip/Chip.jsx
@@ -0,0 +1,17 @@
+import React, { forwardRef } from "react";
+import { classMerge } from "../../../utils/utils";
+
+const Chip = forwardRef(({ className, name, ...props }) => {
+  return (
+    <div
+      className={classMerge(
+        "px-2 bg-neutral-200  text-neutral-500 border border-neutral-300  rounded-[24px] text-[10px] sm:text-xs  font-normal h-[18px] w-fit",
+        className
+      )}
+      {...props}
+    >
+      {name}
+    </div>
+  );
+});
+export default Chip;
