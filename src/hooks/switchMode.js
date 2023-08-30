1a883f6e905a6a9d7bc7e1ff8ee3b5bdd89ac310
Harsh8055 <harshsingh.eth@gmail.com>
2023-08-31 01:29:29 +0530

    light - default

--- src/hooks/switchMode.js
+++ src/hooks/switchMode.js
@@ -5,7 +5,7 @@ export const switchMode = createContext("");
 // Provider
 export const  ThemeProvider  =  ({ children })  =>  {
   // dark and light mode switch
-  const [selectedMode, setSelectedMode] = useState('dark')
+  const [selectedMode, setSelectedMode] = useState('light')
 
 return  (
     <switchMode.Provider value={{ selectedMode, setSelectedMode }}>
