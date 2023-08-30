97f55253505b08bce71f3b7b0f6b474c3d3d8528
Harsh8055 <harshsingh.eth@gmail.com>
2023-08-31 02:35:19 +0530

    testing

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet';
 export default function SEO(
     {title="LinkCollect, Save and Share Resources Blazingly Fast", description="Linkcollect, most loved bookmarking tool for people to collect, curate and share amazing resources", image="https://linkcollect.io/static/media/mainLogo.71d1a218042d27dad79cc550d564c867.svg"}) {
 
+    console.log("-",window.location)
 return (
 <Helmet>
 { /* Standard metadata tags */ }
@@ -11,11 +12,14 @@ return (
 { /* End standard metadata tags */ }
 { /* Facebook tags */ }
 <meta property="og:type" content='website'/>
-<meta property="og:title" content={title} />
+<meta property="og:title" content={window.location.pathname} />
 <meta property="og:description" content={description} />
 <meta property="og:image" content={image} />
 { /* End Facebook tags */ }
-
+<meta
+      property="og:url"
+      content={window.location.href}
+    />
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:title" content={title} />
 <meta property="twitter:description" content={description} />
