2e981e34722317b6236eac97aa1d38707f86b6b1
Amrut Wali <amrutwali@gmail.com>
2023-09-03 08:42:56 +0530

    Minor bug-fix link to extension install not working

--- src/components/Sections/LandingPageV2/Navbar.jsx
+++ src/components/Sections/LandingPageV2/Navbar.jsx
@@ -23,9 +23,9 @@ const Navbar = () => {
         <a href="#home" rel="noreferrer">Home</a>
         <a href="#features" rel="noreferrer">Feature</a>
         <a href="#pricing" rel="noreferrer">Pricing</a>
-        <a href={links.Contact} rel="noreferrer">Contact Us</a>
+        <a href={links.contact} rel="noreferrer">Contact Us</a>
         <button onClick={() => {navigate('/login')}} className="p-[0.75rem] w-[150px] h-min leading-[calc(1.25rem-4px)] border-2 border-primary-300 rounded-[5px]">Log In</button>
-        <button onClick={() => {window.open(links.ExtensionUrl, "_blank")}} className="py-[0.75rem] px-[1rem] w-[175px] h-min leading-[1.25rem] bg-primary-400 rounded-[5px] text-white">Install Extension</button>
+        <button onClick={() => {window.open(links.extensionUrl, "_blank")}} className="py-[0.75rem] px-[1rem] w-[175px] h-min leading-[1.25rem] bg-primary-400 rounded-[5px] text-white">Install Extension</button>
       </nav>
     </header>
   )
