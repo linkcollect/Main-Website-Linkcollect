ff8b2d358f0c5c2e6fad177380afee9a0961b19c
Amrut Wali <amrutwali@gmail.com>
2023-09-10 01:44:19 +0530

    add dark mode to moreByUser page

--- src/components/Sections/Bookmarks/MoreFromUser.jsx
+++ src/components/Sections/Bookmarks/MoreFromUser.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { getAllByUsername } from "../../../api-services/collectionService";
 import CollectionitemV2 from "../../Common/CollectionCard";
 import Carousel from "../LandingPageV2/components/Carousel";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { switchMode } from "../../../hooks/switchMode";
 
 const MoreFromUser = ({ collectionData, user }) => {
 
@@ -11,7 +11,6 @@ const MoreFromUser = ({ collectionData, user }) => {
     const [userCollections, setUserCollections] = useState([])
 
     const auth = useSelector((state) => state.auth);
-    const navigate = useNavigate()
 
     useEffect(() => {
         async function fetchUserCollections() {
@@ -32,12 +31,13 @@ const MoreFromUser = ({ collectionData, user }) => {
         
       }
     }, [])
-    
-    console.log(userCollections);
+
+    const { selectedMode } = useContext(switchMode)
 
     return(
-        auth.username !== user && userCollections?.data?.data.length > 1 && <div className="w-full min-h-[200px] border-t-2 border-neutral-300">
-            <div className="flex flex-col w-full justify-items-center items-start pt-[5rem]">
+        auth.username !== user && userCollections?.data?.data.length > 1 && <div className={`w-full min-h-[200px] border-t-2 ${selectedMode === 
+        "dark" ? "border-dark-secondary" : "border-neutral-300"}`}>
+            <div className={`${selectedMode === "dark" ? "text-white" : ""} flex flex-col w-full justify-items-center items-start pt-[5rem]`}>
                 <h2 className="text-[1.75rem] px-[clamp(1rem,5vw,5rem)]">More from <a className="text-[1.75rem] text-primary-500" href={`https://linkcollect.io/${user}`}>{user}</a></h2>
                 <Carousel className={"px-[clamp(1rem,5vw,5rem)] pt-[2rem] pb-[5rem]"} fullWidth={false} isAutoScroll={false}>
                 {userCollections?.data?.data.filter((collection) => {
