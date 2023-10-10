63308f5f3851c7fb84beca575eff73627b8eff70
Harsh8055 <harshsingh.eth@gmail.com>
2023-10-10 12:54:47 +0530

    added analytics

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ import { store } from './store';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from './hooks/switchMode';
 
+import ReactGA from 'react-ga4';
+const trackingId = 'G-6NHCQSCVJP';
+
+ReactGA.initialize([
+  {
+    trackingId: trackingId,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
